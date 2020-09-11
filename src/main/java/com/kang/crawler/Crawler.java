20437b447ada2318e7751ae087d3a19d41299e38
kang <khj0982@sigongmedia.co.kr>
2020-09-11 13:33:30 +0900

    - 크롤링 테스트 추가

--- src/main/java/com/kang/crawler/Crawler.java
+++ src/main/java/com/kang/crawler/Crawler.java
@@ -2,6 +2,8 @@ package com.kang.crawler;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,5 +20,17 @@ public class Crawler {
 
 	protected Log Logger = LogFactory.getLog(getClass());
 	
+	public void Crawler() throws Exception {
+		Logger.debug("=================== Crawler :" );
+		
+		Document google = Jsoup.connect("https://www.google.com/").get();
+		
+		String html = google.html();
+		Logger.debug("html ============================================" );
+		Logger.debug(html);
+
+		Logger.debug("text ============================================" );
+		Logger.debug(google.text());
+	}
 	
 }
--- src/main/java/com/kang/main/web/MainController.java
+++ src/main/java/com/kang/main/web/MainController.java
@@ -2,8 +2,11 @@ package com.kang.main.web;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kang.crawler.Crawler;
 
 /**
  * <pre>
@@ -19,8 +22,20 @@ public class MainController {
 
 	protected Log Logger = LogFactory.getLog(getClass());
 	
+	@Autowired
+	private Crawler crawler; 
+	
 	@RequestMapping("/")
 	private String main() throws Exception{
 		return "main";
 	}
+	
+	
+	@RequestMapping("/crawler")
+	private String crawler() throws Exception{
+		crawler.Crawler();
+		return "crawler/crawler";
+	}
+	
+	
 }
